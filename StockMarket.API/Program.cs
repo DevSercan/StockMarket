f21faaddf9da4883e9b139cf4455e15e976023b4
Zhedlin <zhedlin@hotmail.com>
2023-12-09 18:27:06 +0300

    09.12.2023

--- StockMarket.API/Program.cs
+++ StockMarket.API/Program.cs
@@ -45,7 +45,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "StockMarketAPI", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "StockMarket", Version = "v1" });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Name = "Authorization",
@@ -79,7 +79,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "StockMarketAPI v1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "StockMarket v1");
     });
 }
 
